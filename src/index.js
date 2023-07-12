c801be5be4584ba194a469f5f910080b8c3dabee
Abdomes <91058164+Abdomes@users.noreply.github.com>
2023-07-12 14:46:26 +0500

    add deploy scripts

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import store from '@store/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { REPO_NAME } from '@constants/repo';
 import App from '@containers/App';
 import ThemeProvider from '@context/ThemeProvider';
 
@@ -11,7 +12,7 @@ import '@styles/index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={`/${REPO_NAME}/`}>
       <Provider store={store}>
         <ThemeProvider>
           <App />
